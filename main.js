ed18d7240fe38d7f40627f045d83bb1f674b0817
mzloria <mikeeloria@gmail.com>
2020-02-21 15:30:13 -0800

    Added description and added HTML description.

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-const fieldSize = 800
-const numberOfCellsInRow = 50
-const framesPerSecond = 8
-
-/*
-const getRandomGrid = () => {
-  const grid = new Array(numberOfCellsInRow)
-  for (let i = 0; i < grid.length; i++) {
-    grid[i] = new Array(numberOfCellsInRow)
-    for (let j = 0; j < grid.length; j++) {
-      grid[i][j] = Math.floor(Math.random() * 2)
-    }
-  }
-  return grid
-}*/
-
-// My code inspired by him
 var canvas;
 var ctx;
 var gameGrid; 
 var numColumns;
 var numRows;
 var resolution = 20;
+var framesPerSecond = 4;
+
+window.onload = function () {
+  canvas = document.getElementById("gameoflife");
+  ctx = canvas.getContext('2d');
+  ctx.canvas.addEventListener("click", function (e) {
+    if(gameGrid[Math.floor((e.clientX - 10)/ resolution)][Math.floor((e.clientY - 10) / resolution)] === 1) {
+      gameGrid[Math.floor((e.clientX - 10) / resolution)][Math.floor((e.clientY - 10) / resolution)] = 0;
+    } else {
+      gameGrid[Math.floor((e.clientX - 10) / resolution)][Math.floor((e.clientY - 10) / resolution)] = 1;
+    }
+  }, false);
+  setUpGameGrid();
+  drawFrame();
+}
+
+function drawFrame() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  draw();
+  setTimeout(() => {requestAnimationFrame(() => drawFrame())}, 1000 / framesPerSecond);
+}
 
 function setUpGameGrid() {
   numColumns = canvas.width / resolution;
@@ -56,97 +60,36 @@ function draw() {
       ctx.strokeRect(x, y, resolution, resolution);
     }
   }
-}
 
-// END CODE
-const getNextGeneration = (grid) => {
-  const nextGrid = new Array(grid.length)
-  for (let i = 0; i < grid.length; i++) {
-    nextGrid[i] = new Array(grid.length)
-    for (let j = 0; j < nextGrid[i].length; j++) {
-      const value = grid[i][j]
-      const neighbors = countNeighbors(grid, i, j)
-      if (value === 0 && neighbors === 3) {
-        nextGrid[i][j] = 1
-      } else if (
-        (value === 1) &&
-        (neighbors < 2 || neighbors > 3)
-      ) {
-        nextGrid[i][j] = 0
+  var nextGeneration = generateArray(numColumns, numRows);
+  for (var i = 0; i < numColumns; i++) {
+    for(var j = 0; j < numRows; j++) {
+      var state = gameGrid[i][j];
+      var neighbors = countNeighbors(gameGrid, i, j);
+
+      if(state === 0 && neighbors === 3) {
+        nextGeneration[i][j] = 1;
+      } else if (state === 1 && (neighbors < 2 || neighbors > 3)) {
+        nextGeneration[i][j] = 0;
       } else {
-        nextGrid[i][j] = value
+        nextGeneration[i][j] = state;
       }
+
     }
   }
-  return nextGrid
+  gameGrid = nextGeneration;
 }
 
-const countNeighbors = (grid, x, y) => {
-  let sum = 0
-  const numberOfRows = grid.length
-  const numberOfCols = grid[0].length
-  for (let i = -1; i < 2; i++) {
-    for (let j = -1; j < 2; j++) {
-      const row = (x + i + numberOfRows) % numberOfRows
-      const col = (y + j + numberOfCols) % numberOfCols
-      sum += grid[row][col]
+function countNeighbors(array, x, y) {
+  var numNeighbors = 0;
+  for(var i = -1; i < 2; i++) {
+    for(var j = -1; j < 2; j++) {
+      let column = (x + i + numColumns) % numColumns;
+      let row = (y + j + numRows) % numRows;
+
+      numNeighbors += array[column][row];
     }
   }
-  sum -= grid[x][y]
-  return sum
-}
-
-const cellStrokeColor = '#aaa'
-const cellSize = fieldSize / numberOfCellsInRow
-
-// DEPRECATED
-const drawGrid = (ctx, grid) => {
-  ctx.strokeStyle = cellStrokeColor
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid.length; j++) {
-      const value = grid[i][j]
-      if (value) {
-        ctx.fillRect(
-          i * cellSize,
-          j * cellSize,
-          cellSize,
-          cellSize,
-        )
-      }
-      ctx.strokeRect(
-        i * cellSize,
-        j * cellSize,
-        cellSize,
-        cellSize,
-      )
-    }
-  }
-}
-// END DEPRECATED
-
-const generation = (ctx, grid) => {
-  ctx.clearRect(0, 0, fieldSize, fieldSize)
-  drawGrid(ctx, grid)
-  const gridOfNextGeneration = getNextGeneration(grid)
-  setTimeout(() => {
-    requestAnimationFrame(() => generation(ctx, gridOfNextGeneration))
-  }, 1000 / framesPerSecond)
-
-}
-
-/*
-window.onload = () => {
-  const canvas = document.getElementById('canvas')
-  const ctx = canvas.getContext('2d')
-  const grid = getRandomGrid()
-  generation(ctx, grid)
-}
-*/
-
-window.onload = function () {
-  canvas = document.getElementById("gameoflife");
-  ctx = canvas.getContext('2d');
-  setUpGameGrid();
-  draw();
- // generation(ctx, gameGrid)
+  numNeighbors -= array[x][y];
+  return numNeighbors;
 }
